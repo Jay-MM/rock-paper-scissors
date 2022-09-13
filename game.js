9f6829a47ee114c85e35f3c84ce2fdd4c1468bfe
Jesus M <marquez.jay444@gmail.com>
2022-09-12 19:38:07 -0500

    pseudo code

--- game.js
+++ game.js
@@ -1,1 +1,25 @@
-console.log('YO')
+//  define array for choices [r, p, s]
+// define var fir computerChoice  
+// define var for playerChoice
+// define var for wins
+// define var for losses
+// define var for ties
+
+// prompt humanChoice
+    // if humanChoice is p and computerChoice is r
+        //humanChoice wins
+    // if humanChoice is r and computerChoice is p
+        //computerChoice wins
+
+// randomly choose computerChoice
+
+// cmopare choices
+
+// display (alert) comparison results (won, tied, lost round)
+
+// show stats(number of wins. losses, ties)
+
+// play again?
+    // restart game
+//else
+    // end game
